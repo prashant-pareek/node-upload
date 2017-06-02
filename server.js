7d4f2960f6a8c80a641bcdd79ca6c9b5b6b681b8
Prashant Pareek <prashantpareek@dotsquares.com>
2017-06-02 18:16:49 +0530

    described the server code

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+// load http module from node js library
+// and assign its object and method to
+// http variable (variable name not required to be same as module name)
 var http = require('http');
 
 function onRequest(request, response) {
+	// message will print two times if browser will request
+	// for favicon too
+	console.log('Request Received');
+
+	// writes header to send to browser
 	response.writeHead(200, {'Content-Type': 'text/plain'});
+
+	// send text
 	response.write('Hello World');
+
+	// finish the response
 	response.end();
 }
 
+// start server and call callback function
+// onRequest, passing request and response object
 http.createServer(onRequest).listen(8888);
+
+// output message immediately on starting the file
+// execution
+console.log('Server has started');
