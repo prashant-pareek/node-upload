8199c2221e64574c444a4e45810dbed4a8554471
Prashant Pareek <prashantpareek@dotsquares.com>
2017-06-02 18:50:10 +0530

    converted http server code into module to make it execute from other parts of application

--- server.js
+++ server.js
@@ -3,25 +3,29 @@
 // http variable (variable name not required to be same as module name)
 var http = require('http');
 
-function onRequest(request, response) {
-	// message will print two times if browser will request
-	// for favicon too
-	console.log('Request Received');
+function start() {
+	function onRequest(request, response) {
+		// message will print two times if browser will request
+		// for favicon too
+		console.log('Request Received');
 
-	// writes header to send to browser
-	response.writeHead(200, {'Content-Type': 'text/plain'});
+		// writes header to send to browser
+		response.writeHead(200, {'Content-Type': 'text/plain'});
 
-	// send text
-	response.write('Hello World');
+		// send text
+		response.write('Hello World');
 
-	// finish the response
-	response.end();
+		// finish the response
+		response.end();
+	}
+
+	// start server and call callback function
+	// onRequest, passing request and response object
+	http.createServer(onRequest).listen(8888);
+
+	// output message immediately on starting the file
+	// execution
+	console.log('Server has started');
 }
 
-// start server and call callback function
-// onRequest, passing request and response object
-http.createServer(onRequest).listen(8888);
-
-// output message immediately on starting the file
-// execution
-console.log('Server has started');
+exports.start = start;
