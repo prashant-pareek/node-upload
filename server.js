7cba639a43e501fc785eda415fd3661291c67ac5
Prashant Pareek <prashantpareek@dotsquares.com>
2017-06-05 18:27:19 +0530

    modified application to upload single png file and show it

--- server.js
+++ server.js
@@ -8,18 +8,8 @@ var url = require('url');
 
 function start(route, handle) {
 	function onRequest(request, response) {
-		var postData = '';
 		var pathname = url.parse(request.url).pathname;
-
-		request.setEncoding('utf8');
-
-		request.addListener('data', function(postDataChunk) {
-			postData += postDataChunk;
-		});
-
-		request.addListener('end', function() {
-			route(handle, pathname, response, postData);
-		})
+		route(handle, pathname, response, request);
 
 		// message will print two times if browser will request
 		// for favicon too
